277c08f137dc1fef7d3729d680644cd6e09978ec
paliwal90 <piyushpaliwal90@gmail.com>
2017-01-11 18:26:52 +0200

    Geo search query to es5

---
+++ geo-search-query-to-es.py
@@ -0,0 +1,54 @@
+from elasticsearch import Elasticsearch
+
+import time
+import pandas as pd
+
+start_time = time.time()
+
+ES_INDEX = 'poi'
+ES_TYPE = 'poi_elastic'
+FILE_PATH = "/home/paliwal/geo/poi/sampled_locations.csv"
+
+es = Elasticsearch([{'host': 'xxx.xxx.xxx.xx', 'port': xxxx}])
+
+def query(coor):
+  res = es.search(index=ES_INDEX, doc_type=ES_TYPE, body={
+    "query": {
+          "bool" : {
+              "must" : {
+                  "match_all" : {}
+              },
+              "filter" : {
+                  "geo_distance" : {
+                      "distance" : "5km",
+                      "location" : coor
+                  }
+              }
+          }
+      }
+  })
+  
+  #change it to retrive whatever desired content
+  #for doc in res['hits']['hits']:
+    #print(doc)
+  print("%d documents" % res['hits']['total'])
+
+  query_time = time.time() - start_time
+  print("--- %s milliseconds ---" % int(round(query_time * 1000))) 
+  
+def read_sampled_loc(file_path):  
+  coors = pd.read_csv(file_path, sep="\t") #to not sep by ',' due to entries(locations) having ','
+  return coors
+
+
+if __name__ == '__main__':
+  
+  coors = read_sampled_loc(FILE_PATH)
+
+  for coor in coors.ix[:,0]:
+    start_time = time.time()
+    print(coor)
+    query(coor)
+  
+
+
